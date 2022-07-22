e860e6b487adbdda5f9008cef05f40792cdd2ae4
Wataru <wata_2@icloud.com>
2022-07-22 12:16:48 +0900

    CHANGE

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tasks',
-    'bootstrap4'
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -65,7 +65,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'builtins':[
+            'builtins': [
                 'bootstrap4.templatetags.bootstrap4',
             ],
         },
